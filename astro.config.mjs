2b8b19ab8c901aa33ed4333235ae64ada64f4a3d
akhlakm <me@akhlakm.com>
2026-04-23 16:46:36 -0400

    Add navbar link, compress html outputs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,11 @@ import tailwind from "@astrojs/tailwind";
 import { template } from "./src/settings";
 
 import sitemap from "@astrojs/sitemap";
+import compress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind(), sitemap()],
+    integrations: [react(), tailwind(), sitemap(), compress()],
     site: template.website_url,
     base: template.base,
 });
